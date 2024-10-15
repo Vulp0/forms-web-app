85372b996c5c9265189a967efaa8d53a8e61da77
Mau <mauriciomartinezvazquez@rocketmail.com>
2024-10-15 09:58:55 -0500

    something

--- client/src/Signup.tsx
+++ client/src/Signup.tsx
@@ -12,17 +12,17 @@ function Signup() {
                     <div className="inputFieldContainer">
                         <div className="combo">
                             <label className="labelText" htmlFor="Name">Name</label>
-                            <input className="inputField" type="text" name="Name" id="NameInput" placeholder="Your name" />
+                            <input className="inputField" type="text" name="Name" id="NameInput" placeholder="Your name"/>
                         </div>
 
                         <div className="combo">
                             <label className="labelText" htmlFor="Email">Email</label>
-                            <input className="inputField" type="email" name="Email" id="EmailInput" placeholder="Your email address" />
+                            <input className="inputField" type="email" name="Email" id="EmailInput" placeholder="Your email address"/>
                         </div>
 
                         <div className="combo">
                             <label className="labelText" htmlFor="Password">Password</label>
-                            <input className="inputField" type="password" name="Password" id="PasswordInput" placeholder="Your password"/>
+                            <input className="inputField" type="password" name="Password" id="PasswordInput" placeholder="Password must be 8 characters or longer"/>
                         </div>
                     </div>
                     
